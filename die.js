bdda31707052c2551d83285fee433fb98052a774
linkin2b <b0skiewheels@gmail.com>
2021-09-23 14:36:57 -0700

    initial commit

---
+++ die.js
@@ -0,0 +1,36 @@
+//establish die paramenters
+
+const dFour = [1, 2 , 3 ,4];
+const dSix = [1, 2, 3, 4, 5, 6];
+const dEight = [1, 2, 3, 4, 5, 6, 7, 8];
+const dTen = [1, 2, 3 , 4, 5, 6, 7, 8, 9, 10];
+const dTwelve = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const dTwenty = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 12, 14, 15, 16, 17, 18, 19, 20];
+
+class Roller extends React.component
+constructor(props) {
+	super(props);
+	this.state ={
+		dice='';
+		currentRoll = ' ';
+	};
+	this.playSound = this.playSound.bind.this;
+	this.onPress = this.onPress.bind.this;
+}
+
+//random roll withing die parameters
+
+function roll (dice){
+	return dice[Math.floor((Math.random()*dice.length))];
+
+}
+//html button to initiate roll 
+
+
+
+//radio buttons to select die
+
+
+
+//reulting roll display
+
