3c93b784a236fb89269c0c3165e1b05201625d31
linkin2b <b0skiewheels@gmail.com>
2021-11-02 13:45:39 -0700

    progress1

--- dice-roller/src/App.js
+++ dice-roller/src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ReactDOM } from "react-dom";
+import './src/App.css';
+
+//establish die paramenters
+const die= [
+  {},
+  {},
+
+]
+const dFour = [1, 2 , 3 ,4];
+const dSix = [1, 2, 3, 4, 5, 6];
+const dEight = [1, 2, 3, 4, 5, 6, 7, 8];
+const dTen = [1, 2, 3 , 4, 5, 6, 7, 8, 9, 10];
+const dTwelve = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const dTwenty = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 12, 14, 15, 16, 17, 18, 19, 20];
+
+class Roller extends React.component
+
+constructor(props) {
+	super(props);
+
+	this.state ={
+		dice:'',
+		currentRoll: ''
+	};
+	this.playSound = this.playSound.bind.this;
+	this.onPress = this.onPress.bind.this;
+};
+
+
+//random roll withing die parameters
+
+roll(dice){
+	return dice[Math.floor((Math.random()*dice.length))];
+
+};
+//html button to initiate roll 
+
+//radio buttons to select die
+
+
+//reulting roll display
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <button onclick={() =>
+        this.setState(dice.dFour)}>Select Die</button>
+      <button onclick={() =>
+        this.setState(dice.dSix)}>Select Die</button>
+      <button onclick={() =>
+        this.setState(dice.dEight)}>Select Die</button>
+      <button onclick={() =>
+        this.setState(dice.dTen)}>Select Die</button>
+      <button onclick={() =>
+        this.setState(dice.dTwelve)}>Select Die</button>
+      <button onclick={() =>
+        this.setState(dice.dTwenty)}>Select Die</button>
+   </div>
   );
 }
 
--- dice-roller/src/index.js
+++ dice-roller/src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './src/index.css';
+import './src/App.js';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+
+ReactDOM.render('./src/App.js');
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
