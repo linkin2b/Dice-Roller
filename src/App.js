ff372906ec1eb21dde39154acfaae3093927d90b
linkin2b <b0skiewheels@gmail.com>
2022-02-05 17:54:42 -0800

    2-5-22 post img fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import './App.css';
+import d4 from './imgs/d4.png'
+import d6 from './imgs/d6.png'
+import d8 from './imgs/d8.png'
+import d10 from './imgs/d10.png'
+import d12 from './imgs/d12.png'
 import d20 from './imgs/d20.png'
+import d100 from './imgs/d100.png'
 
-//establish die paramenters
-// const die= [
-//   {},
-//   {},
-//
-// ]
+
 const dFour = [1, 2, 3, 4];
 const dSix = [1, 2, 3, 4, 5, 6];
 const dEight = [1, 2, 3, 4, 5, 6, 7, 8];
@@ -26,7 +27,7 @@ class Roller extends React.Component {
       dice: dTwenty, // Sets an initial state
       currentRoll: '',
       rollLog: [],
-      diceImg: "../src/imgs/d20.png",
+      diceImg: d20,
       totalRoll: [],
       sumRoll: 0
     };
@@ -56,7 +57,7 @@ class Roller extends React.Component {
 
   clear() {
     this.setState(() => {
-      return {rollLog: [], currentRoll: '', totalRoll: 0};
+      return {rollLog: [], currentRoll: '', totalRoll: 0, sumRoll: 0};
     })
   }
 
@@ -64,19 +65,19 @@ class Roller extends React.Component {
   getDiceImage(dice) {
     console.log(dice)
     if (dice === dFour)
-      return "imgs/d4.png";
-    if (dice === dSix.length)
-      return "./imgs/d6.png";
-    if (dice === dEight.length)
-      return "./imgs/d8.png";
-    if (dice === dTen.length)
-      return "./imgs/d10.png";
-    if (dice === dTwelve.length)
-      return "./imgs/d12.png";
+      return d4;
+    if (dice === dSix)
+      return d6;
+    if (dice === dEight)
+      return d8;
+    if (dice === dTen)
+      return d10;
+    if (dice === dTwelve)
+      return d12;
     if (dice === dTwenty)
       return d20;
-    if (dice === dHundred.length)
-      return "./imgs/d100.png";
+    if (dice === dHundred)
+      return d100;
   }
 
 
@@ -90,38 +91,38 @@ class Roller extends React.Component {
           <div id="dieTray" onChange={this.handleChange}>
             <div>
               <button id="d4" onClick={() =>
-                  this.setState({dice: dFour, diceImg: "../src/imgs/d4.png"})}>
+                  this.setState({dice: dFour, diceImg:d4})}>
               </button>
             </div>
             <div>
               <button id="d6" onClick={() =>
-                  this.setState({dice: dSix})}>
+                  this.setState({dice: dSix, diceImg:d6})}>
 
               </button>
             </div>
             <div>
               <button id="d8" onClick={() =>
-                  this.setState({dice: dEight})}>
+                  this.setState({dice: dEight, diceImg:d8})}>
               </button>
             </div>
             <div>
               <button id="d10" onClick={() =>
-                  this.setState({dice: dTen})}>
+                  this.setState({dice: dTen, diceImg:d10})}>
               </button>
             </div>
             <div>
               <button id="d12" onClick={() =>
-                  this.setState({dice: dTwelve})}>
+                  this.setState({dice: dTwelve, diceImg:d12})}>
               </button>
             </div>
             <div>
               <button id="d20" onClick={() =>
-                  this.setState({dice: dTwenty})}>
+                  this.setState({dice: dTwenty, diceImg:d20})}>
               </button>
             </div>
             <div>
               <button id="d100" onClick={() =>
-                  this.setState({dice: dHundred})}>
+                  this.setState({dice: dHundred, diceImg:d100})}>
               </button>
             </div>
           </div>
